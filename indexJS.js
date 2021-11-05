aa20d642eb48e69a96edffea161e2d1930e9d1dd
Sparsh Raj <sparshraj6a@gmail.com>
2021-11-06 01:22:13 +0530

    final

--- indexJS.js
+++ indexJS.js
@@ -1,4 +1,19 @@
-if(document.getElementById("pincode").innerHTML)
-{
-    alert("Invalid input!");
+ var pinform=document.getElementById("formInput")
+
+var e = document.getElementById("pincode1");
+
+function numberSelected(event) {
+  var strUser = e.value;
+  event.preventDefault();
+    if(strUser.length==6){
+        location.href="./index2.html"
+    }
+    else{
+        alert("Invalid Input!")
+    }
+  console.log("button", strUser);
+//   containerSelected(strUser);
+
 }
+
+pinform.addEventListener("submit",numberSelected)
